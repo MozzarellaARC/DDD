80350202ad72fc8c0558e954cbbf09b7fa626b2e
MozzaellaARC <mozzarellah.g@gmail.com>
2026-05-02 22:05:09 +0700

    untrack things

--- cmake_build_and_debug.sh
+++ cmake_build_and_debug.sh
@@ -1,4 +1,4 @@
 cmake --build ./build --parallel
-./build/LearnCpp.exe
+./build/DDD.exe
 
 echo
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
+#include <chrono>
 #include <json.hpp>
+#include <print>
 #include <string_view>
 using json = nlohmann::json;
 
@@ -27,75 +29,39 @@ fs::path windowsRelativePath() {
 }
 
 // Parse json
-json parseJson(std::string x) {
-  std::ifstream f(windowsRelativePath().parent_path() / "dir.json");
-  return json::parse(f)[x];
-}
-
-// Exclude json
-json excludeJson(std::string x) {
-  std::ifstream f(windowsRelativePath().parent_path() / "exclude.json");
-  return json::parse(f)[x];
-}
-
-// Mumbo jumbo
-std::string matchExclude() {
-  const char *a{"test"};
-  std::string s = a;
-  return static_cast<std::string_view>(a);
+json parseJson() {
+  std::ifstream f(windowsRelativePath().parent_path().parent_path() /
+                  "dir.json");
+  return json::parse(f);
 }
 
 // Json iterator
-void iterateJson(bool a) {
-  json fromJson = a ? parseJson("source") : parseJson("target");
-  json toJson = a ? parseJson("target") : parseJson("source");
+void iterateJson(bool a) {}
 
-  for (auto &[key, fromPath] : fromJson.items()) {
-    auto &toPath = toJson[key];
+int main(int argc, char *argv[]) {
+  std::string userOptions = argv[1];
 
-    fs::path from = fromPath.get<std::string>();
-    fs::path to = toPath.get<std::string>();
+  std::string sourceDir{};
+  std::string targetDir{};
 
-    if (!fs::exists(from)) {
-      std::cout << from << " -> " << "source directory doesn't exists\n";
-    } else if (fs::is_empty(from)) {
-      std::cout << from << " -> " << "source directory is empty\n";
-    } else if (!fs::exists(to)) {
-      std::cout << to << " -> " << "target directory not exists\n";
-    } else {
-      for (auto &entry : fs::directory_iterator(from)) {
-        // std::cout << fs::path(entry).generic_string() << '\n';
-        std::string genericPath = from.generic_string();
-        // auto exclusion[];
-
-        // if (fs::path(entry.path()) == "abc2") {
-        //   std::cout << "badabings";
-        // }
+  json jsonObject{parseJson()};
+  for (auto &[key, val] : jsonObject.items()) {
+    for (auto &[source, sourceVal] : val.items()) {
+      // source/target level
+      std::println("{} : ", source);
+      // app/dir level
+      if (source == "source") {
+        for (auto &[app, dir] : sourceVal.items()) {
+          std::cout << dir << '\n';
+          sourceDir = dir;
+        }
       }
-
-      // std::cout << from << " -> " << to << '\n';
     }
   }
-}
 
-// Let's go (Interface)
-int main() {
-  std::cout << "Would you like to Retrieve or Deploy dotfiles \n"
-            << "0: Retrieve - 1: Deploy\n";
-
-  int answer;
-  do {
-    std::cin >> answer;
-    if (answer == 0) {
-      std::cout << "Retrieving user data\n";
-      iterateJson(true);
-    } else if (answer == 1) {
-      std::cout << "Deploying user data\n";
-      iterateJson(false);
-    } else {
-      std::cout << "Please enter 0 or 1\n";
-    }
-  } while (answer != 0 && answer != 1);
+  std::cout << '\n';
+  std::cout << sourceDir;
+  std::cout << targetDir;
 
   return 0;
 }
