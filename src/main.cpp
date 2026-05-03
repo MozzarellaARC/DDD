24fde48db602fd5bb6885cf140a4671c843d7303
MozzaellaARC <mozzarellah.g@gmail.com>
2026-05-03 22:58:28 +0700

    working exclusion and inclusion

--- src/main.cpp
+++ src/main.cpp
@@ -20,7 +20,6 @@ struct ManagedObject {
   std::vector<std::string> localDirIncl{};
   std::vector<std::string> remoteDir{};
 
-  bool hasInclusion{false};
   std::vector<std::string> exclusion{};
   std::vector<std::string> inclusion{};
 };
@@ -62,84 +61,7 @@ bool doExists(std::string s, std::vector<std::string> v) {
   return false;
 }
 
-void jsonObjectManager(ManagedObject &data) {
-  json jsonObject{parseJson()};
-  for (auto &[key, val] : jsonObject.items()) {
-    for (auto &[from, fromVal] : val.items()) {
-      std::println("{} : ", from);
-
-      // local
-      if (from == "local" && !fromVal.empty()) {
-        for (auto &[app, dir] : fromVal.items()) {
-          data.exclusion.clear();
-          data.inclusion.clear();
-          data.hasInclusion = false;
-
-          if (dir.is_object()) {
-
-            // Pass 1: collect filters
-            for (auto &[innerKey, innerVal] : dir.items()) {
-              if (innerKey == "exc" && !doExists(innerVal, data.exclusion))
-                data.exclusion.push_back(innerVal);
-              if (innerKey == "incl") {
-                data.hasInclusion = true;
-                if (!doExists(innerVal, data.inclusion))
-                  data.inclusion.push_back(innerVal);
-              }
-            }
-
-            // Pass 2: scan source directories
-            for (auto &[innerKey, innerVal] : dir.items()) {
-              if (innerKey == "src") {
-                // localDir = innerVal;
-                if (!fs::is_empty(innerVal)) {
-                  std::cout << app << '\n';
-                }
-                for (auto &srcDir : fs::directory_iterator(innerVal)) {
-                  bool check_inclusion =
-                      doExists(srcDir.path().string(), data.inclusion);
-
-                  bool check_exclusion =
-                      doExists(srcDir.path().string(), data.exclusion);
-
-                  if (data.hasInclusion) {
-                    // data.inclusion mode: only print files in inclusion list
-                    if (check_inclusion) {
-                      std::cout << srcDir << '\n';
-                      data.localDirIncl.push_back(srcDir.path().string());
-                    }
-                  } else {
-                    // data.exclusion mode: print everything except excluded
-                    // files
-                    if (!check_exclusion) {
-                      std::cout << srcDir << '\n';
-                      data.localDirExc.push_back(srcDir.path().string());
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-
-      // remote
-      if (from == "remote" && !fromVal.empty()) {
-        for (auto &[app, dir] : fromVal.items()) {
-          if (!fs::is_empty(dir)) {
-            std::cout << app << '\n';
-          }
-          for (auto &[innerKey, innerVal] : dir.items()) {
-            for (auto &srcDir : fs::directory_iterator(innerVal)) {
-              std::cout << srcDir << '\n';
-              data.remoteDir.push_back(srcDir.path().string());
-            }
-          }
-        }
-      }
-    }
-  }
-}
+void jsonObjectManager(ManagedObject &data) {}
 
 // Source - https://stackoverflow.com/a/20303915
 // Posted by masoud, modified by community. See post 'Timeline' for change
@@ -166,45 +88,63 @@ int main(int argc, char *argv[]) {
   std::vector<std::string> localSourceDir{};
   std::vector<std::string> remoteSourceDir{};
 
-  // ManagedObject data{};
-  // jsonObjectManager(data);
+  bool hasIncl{true};
+
+  ManagedObject data{};
+  jsonObjectManager(data);
 
   json jsonObject{parseJson()};
 
-  // needle pass
-  for (auto &[baseKey, baseVal] : jsonObject.items()) {
-    for (auto &[sourceKey, sourceVal] : baseVal.items()) {
-      for (auto &[appKey, appVal] : sourceVal.items()) {
-        if (appVal.is_object()) {
-          for (auto &[innerKey, innerVal] : appVal.items()) {
-            if (innerKey == "incl") {
-              for (auto inclusionElement : innerVal) {
-                jsonNeedleIncl.push_back(inclusionElement);
-              }
-            }
-            if (innerKey == "exc") {
-              for (auto exclusionElement : innerVal) {
-                jsonNeedleExcl.push_back(exclusionElement);
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-
-  // badabings
+  // dir pass
   for (auto &[baseKey, baseVal] : jsonObject.items()) {
     for (auto &[sourceKey, sourceVal] : baseVal.items()) {
       if (sourceKey == "local") {
         for (auto &[appKey, appVal] : sourceVal.items()) {
-          for (auto &[innerKey, innerVal] : appVal.items()) {
-            if (innerKey == "src") {
-              for (auto &srcDir : fs::directory_iterator(innerVal)) {
-                if (in_array(srcDir.path().string(), needle)) {
-                  localSourceDir.push_back(srcDir.path().string());
+          std::cout << appKey << '\n';
+          jsonNeedleIncl.clear();
+          jsonNeedleExcl.clear();
+
+          if (appVal.is_object()) {
+            // filter pass
+            for (auto &[innerKey, innerVal] : appVal.items()) {
+              if (innerKey == "incl") {
+                for (auto inclusionElement : innerVal) {
+                  jsonNeedleIncl.push_back(inclusionElement);
+                  hasIncl = true;
                 }
               }
+              if (innerKey == "exc") {
+                for (auto exclusionElement : innerVal) {
+                  jsonNeedleExcl.push_back(exclusionElement);
+                  hasIncl = false;
+                }
+              }
+            }
+
+            // directory pass
+            for (auto &[innerKey, innerVal] : appVal.items()) {
+              if (innerKey == "src") {
+                for (auto &srcDir : fs::directory_iterator(innerVal)) {
+                  if (in_array(srcDir.path().string(), jsonNeedleIncl) &&
+                      hasIncl) {
+                    std::cout << srcDir << '\n';
+                    localSourceDir.push_back(srcDir.path().string());
+                  }
+
+                  if (!in_array(srcDir.path().string(), jsonNeedleExcl) &&
+                      !hasIncl) {
+                    std::cout << srcDir << '\n';
+                    localSourceDir.push_back(srcDir.path().string());
+                  }
+                }
+              }
+            }
+          }
+
+          // pure dir
+          if (!appVal.is_object()) {
+            for (auto &srcDir : fs::directory_iterator(appVal)) {
+              std::cout << srcDir << '\n';
             }
           }
         }
@@ -212,37 +152,37 @@ int main(int argc, char *argv[]) {
     }
   }
 
-  // movement from remote doesn't need exclusive check
-  for (auto &[baseKey, baseVal] : jsonObject.items()) {
-    for (auto &[sourceKey, sourceVal] : baseVal.items()) {
-      if (sourceKey == "remote") {
-        for (auto &[appKey, appVal] : sourceVal.items()) {
-          for (auto &[innerKey, innerVal] : appVal.items()) {
-            for (auto &srcDir : fs::directory_iterator(innerVal)) {
-              if (in_array(srcDir.path().string(), needle)) {
-                std::cout << srcDir << '\n';
-              }
-            }
-          }
-        }
-      }
-    }
-  }
+  // // movement from remote doesn't need exclusive check
+  // for (auto &[baseKey, baseVal] : jsonObject.items()) {
+  //   for (auto &[sourceKey, sourceVal] : baseVal.items()) {
+  //     if (sourceKey == "remote") {
+  //       for (auto &[appKey, appVal] : sourceVal.items()) {
+  //         for (auto &[innerKey, innerVal] : appVal.items()) {
+  //           for (auto &srcDir : fs::directory_iterator(innerVal)) {
+  //             if (in_array(srcDir.path().string(), needle)) {
+  //               // std::cout << srcDir << '\n';
+  //             }
+  //           }
+  //         }
+  //       }
+  //     }
+  //   }
+  // }
 
   //
   // for (auto &juxtapose : localSourceDir) {
   //   std::cout << juxtapose << '\n';
   // }
 
-  std::cout << "needle inlcude\n";
-  for (auto &juxtapose : jsonNeedleIncl) {
-    std::cout << juxtapose << '\n';
-  }
-
-  std::cout << "needle exclude\n";
-  for (auto &juxtapose : jsonNeedleExcl) {
-    std::cout << juxtapose << '\n';
-  }
+  // std::cout << "needle inlcude\n";
+  // for (auto &juxtapose : jsonNeedleIncl) {
+  //   std::cout << juxtapose << '\n';
+  // }
+  //
+  // std::cout << "needle exclude\n";
+  // for (auto &juxtapose : jsonNeedleExcl) {
+  //   std::cout << juxtapose << '\n';
+  // }
 
   return 0;
 }
